68ab8ea72590e0145c8e6c9cce05152d98344a3d
Jorge1911 <pollo.19xoco@gmail.com>
2022-11-08 11:00:37 -0600

    1.2

--- 01-crud/app.js
+++ 01-crud/app.js
@@ -15,3 +15,54 @@ const cargarPosts=async()=>{
         </tr>`;
     })
 }
+
+const addPost=async()=>{
+    let titulo=document.querySelector("#titulo").value;
+    let body=document.querySelector("#body").value;
+    const post={
+        title:titulo,
+        body:body,
+        userid:1
+    };
+
+
+    const api = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(post),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    
+    
+    });
+
+const respuesta=await api.json();
+tabla=document.querySelector("#lista");
+tr=document.createElement("tr");
+tr.innerHTML=`
+                <th scope="row">${respuesta.id}</th>
+                  <td>${respuesta.title}</td>
+                  <td>${respuesta.body}</td>
+                  <td><button class="btn btn-primary btn-lg"><i class="bi bi-tiktok" data-bs-toggle="modal" data-bs-target="#editModal"></i> Editar </button> </td>
+                  <td> <button class="btn btn-danger btn-lg"><i class="bi bi-trash3-fill" data-bs-toggle="modal" data-bs-target="#deleteModal"></i> Eliminar </button></td>
+               
+`
+tabla.appendChild(tr);
+if(respuesta!=null){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'EXITO ',
+        title: 'guardado exitoso',
+        showConfirmButton: false,
+        timer: 1500
+      })
+}else{
+    Swal.fire(
+        'Eres tonto?',
+        'That thing is still around?',
+        'question'
+        )
+
+
+}
+}
