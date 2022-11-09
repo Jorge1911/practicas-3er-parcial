7cbe47070d96d13f863ea02f6866d5a5fbc36f84
Jorge1911 <pollo.19xoco@gmail.com>
2022-11-09 09:31:13 -0600

    1.2

--- 01-crud/app.js
+++ 01-crud/app.js
@@ -1,4 +1,4 @@
-const cargarPosts=async()=>{
+const cargarPosts=async(id)=>{
     let url="https://jsonplaceholder.typicode.com/posts"
     const api= await fetch(url);
     const data=await api.json();
@@ -10,11 +10,65 @@ const cargarPosts=async()=>{
         <th scope="row">${item.id}</th>
         <td>${item.title}</td>
         <td>${item.body}</td>
-        <td><button type="button" class="btn btn-secondary btn-lg " data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-brush-fill "></i>EDITAR</button></td>
+        <td><button type="button" class="btn btn-secondary btn-lg " data-bs-toggle="modal" data-bs-target="#editModal" onclick="cargarPost(${item.id})"><i class="bi bi-brush-fill "></i>EDITAR</button></td>
         <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-eraser-fill"></i>ELIMINAR</button></td>
         </tr>`;
     })
 }
+
+const cargarPost=async(id)=>{    
+    const api= await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    const respuesta=await api.json();
+    console.log(respuesta);
+    document.querySelector("#etitle").value=respuesta.title;
+    document.querySelector("#ebody").value=respuesta.body;
+    document.querySelector("#eid").value=respuesta.id;
+    document.querySelector("#euserid").value=respuesta.userId;
+}
+
+const guardarPost=async()=>{
+    let title=document.querySelector("#etitle").value;
+    let body=document.querySelector("#ebody").value;
+    let id=document.querySelector("#eid").value;
+    let userid=document.querySelector("#euserid").value;
+
+   const post={
+title:title,
+body:body,
+id:id,
+user:userid
+}
+    const api= await fetch('https://jsonplaceholder.typicode.com/posts/'+id, {
+    method: 'PUT',
+    body: JSON.stringify(post),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+},
+}
+)
+const respuesta=api.json();
+console.log(respuesta)
+if(respuesta!=null){
+    Swal.fire({
+        position: 'top-end',
+        icon: 'EXITO ',
+        title: 'EDITADO exitoso',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}else{
+    Swal.fire(
+        'Eres tonto?',
+        'That thing is still around?',
+        'question'
+        )
+
+
+}
+
+}
+
+
 
 const addPost=async()=>{
     let titulo=document.querySelector("#titulo").value;
@@ -29,9 +83,9 @@ const addPost=async()=>{
     const api = await fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
         body: JSON.stringify(post),
-      headers: {
+        headers: {
         'Content-type': 'application/json; charset=UTF-8',
-      },
+    },
     
     
     });
@@ -41,11 +95,10 @@ tabla=document.querySelector("#lista");
 tr=document.createElement("tr");
 tr.innerHTML=`
                 <th scope="row">${respuesta.id}</th>
-                  <td>${respuesta.title}</td>
-                  <td>${respuesta.body}</td>
-                  <td><button type="button" class="btn btn-secondary btn-lg " data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-brush-fill "></i>EDITAR</button></td>
+                    <td>${respuesta.title}</td>
+                    <td>${respuesta.body}</td>
+                    <td><button type="button" class="btn btn-secondary btn-lg " data-bs-toggle="modal" data-bs-target="#editModal"><i class="bi bi-brush-fill "></i>EDITAR</button></td>
         <td><button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-eraser-fill"></i>ELIMINAR</button></td>
-               
 `
 tabla.appendChild(tr);
 if(respuesta!=null){
