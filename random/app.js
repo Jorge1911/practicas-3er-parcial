f9367b8c583f79c91198a35465d3f139671c2c18
Jorge1911 <pollo.19xoco@gmail.com>
2022-11-16 10:23:44 -0600

    1.8

--- random/app.js
+++ random/app.js
@@ -1,26 +1,28 @@
-const cargarRazas=async ()=>{
-    const api=await fetch("https://dog.ceo/api/breeds/list/all");
-    const response=await api.json();
+const cargarRazas=async()=>{
+    const api= await fetch("https://dog.ceo/api/breeds/list/all");
+    const response= await api.json();
     console.log(response.message);
     let comboRazas=document.querySelector("#razas");
     data=response.message;
     Object.keys(data).forEach(key=>{
         comboRazas.innerHTML+=`
-        <option value="${key}">${key}</option>`
-    })
+        <option  value="${key}">${key}</option>
+        `
+    });
 }
 
-const changePerritos=async (raza)=>{
-    
-    const api=await fetch(`https://dog.ceo/api/breeds/${raza}/random`);
-    const response=await api.json();
-    console.log(response.message);
-    let divPerritos=document.querySelector("#perritos");
-    divPerritos.innerHTML="";
-    data=response.message;
-   data.map(imgPerrito=>{
-
+const changePerritos=async(raza)=>{
+const api=await fetch (`https://dog.ceo/api/breed/${raza}/images`);
+const response=await api.json();
+console.log(response.message);
+let divPerritos=document.querySelector("#perritos");
+data=response.message;
+divPerritos.innerHTML="";
+data.map(imgPerrito=>{
     divPerritos.innerHTML+=`
-    <img src="${imgPerrito}">`
-   })
-}
+    <img src=" ${imgPerrito}">
+   
+   
+    `
+})
+}   
