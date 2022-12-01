ac873b5d6523d221b42cc4b0a6ca9015eb68b09d
Jorge1911 <pollo.19xoco@gmail.com>
2022-12-01 07:21:31 -0600

    2.2

--- 06-login/app.js
+++ 06-login/app.js
@@ -1,131 +1,167 @@
-import {app} from './firebase.js'
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, onAuthStateChanged, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { app } from "./firebase.js";
 
-const auth=getAuth(app);
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+
+let user = null;
+
+
+const auth = getAuth(app);
+const provier = new GoogleAuthProvider();
+const btnCrear = document.querySelector("#btnCrear");
+const btnGoogle = document.querySelector("#btnGoogle");
+const btnIniciar = document.querySelector("#btnIniciar");
+const btnCerrar=document.querySelector("#btnCerrar");
+
+btnGoogle.addEventListener('click',async(e)=>{
+  e.preventDefault();
+  try {
+    const credentials= await signInWithPopup(auth,provier)
+    user=credentials.user;
+    const modalInstance= bootstrap.modal.getInstance(btnGoogle.closest('.modal'));
+    modalInstance.hide();
+    checarEstado(user)
+} catch (error) {
+    console.log(error);
+  }
+ 
+})
+const checarEstado=(user=null)=>{
+  console.log(user);
+  if (user==null) {
+    document.querySelector("#iniciar").style.display="block";
+    document.querySelector("#crear").style.display="block";
+    document.querySelector("#btnCerrar").style.display="none";
+  } else {
+    document.querySelector("#iniciar").style.display="none";
+    document.querySelector("#crear").style.display="none";
+    document.querySelector("#btnCerrar").style.display="block";
+  }
+}
+
+btnCerrar.addEventListener('click',async(e)=>{
+  e.preventDefault();
+  try {
+    await  signOut(auth);
+    checarEstado()
+  } catch ( error) {
+    console.log(error)
+  }
+  });
+  
+  
+  
 
 
 
 
 
-const btnIniciarSesion=document.querySelector("#btnIniciarSesion");
-btnIniciarSesion.addEventListener('click', async(e)=>{
-    e.preventDefault();
-    const email=document.querySelector("#iniciarEmail");
-    const password=document.querySelector("#iniciarPassword");
-    console.log(email.value,password.value);
-    try {
-        const res= await signInWithEmailAndPassword(auth, email.value, password.value)
-        console.log(res.user);
-        Swal.fire('bienvenido');
-        const user2=res.user;      
-        var myModalEl = document.getElementById('iniciarModal');
-        var modal = bootstrap.Modal.getInstance(myModalEl)
-        modal.hide();
-        const res2= await onAuthStateChanged(auth,(user2) =>{
-          const container=document.querySelector("#container");
-          if (user2){
-            container.innerHTML=`<h1>${user2.email}</h1>`
-            document.querySelector("#iniciar").style.display="none";
-            document.querySelector("#crear").style.display="none";
-            const uid=user2.uid;
-          
-          }else{
-            container.innerHTML=`<h1>no hay usuario</h1>`
-          }
-        })
-       
-    }catch(error) {
-        Swal.fire('usuario y/o contraseña incorrecta');
-    }
 
+onAuthStateChanged(auth,(user)=>{
+  const container=document.querySelector("#container");
+  checarEstado(user);
+  if (user) {
+container.innerHTML=`<h1>BIENVENIDO: ${user.email}</h1>
+`
+const uid=user.uid;
+
+  } else {
+    container.innerHTML=`<h1>no hay usuarios</h1>`
+  }
+})
+
+
+
+
+
+
+ btnIniciar.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#iniciarEmail");
+  const password = document.querySelector("#iniciarPassword");
+  console.log(email.value, password.value);
+
+   try {
+     const res = await signInWithEmailAndPassword(
+       auth,
+       email.value,
+       password.value
+     );
+     console.log(res.user);
+     const user = res.user;
+     Swal.fire('Bienvenido Nuevamente!!');
+     var myModalEl = document.getElementById("iniciarModal");
+     var modal = bootstrap.Modal.getInstance(myModalEl);
+     modal.hide();
+     const res2 = await onAuthStateChanged(auth, (user) => {
+       const container = document.querySelector("#container");
+       if (user) {
+         container.innerHTML = `<h1>${user.email} </h1>`;
+         document.querySelector("#iniciar").style.display = "none";
+         document.querySelector("#crear").style.display = "none";
+         const uid = user.uid;
+       } else {
+       container.innerHTML = `<h1>No Hay Usuarios!!</h1>`;
+       }
+     });
+   } catch (error) {
+     Swal.fire("Usuario y/o Password Incorrectos");
+   }
+   bootstrap.Modal.getInstance(document.getElementById('iniciarModal')).hide();
 });
 
+btnCrear.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#crearEmail");
+  const password = document.querySelector("#crearPassword");
+  console.log(email.value, password.value);
+  var myModalEl = document.getElementById("crearModal");
+  var modal = bootstrap.Modal.getInstance(myModalEl);
 
-const btnCrearCuenta = document.querySelector("#btnCrearCuenta");
-btnCrearCuenta.addEventListener('click', async(e)=>{
-    e.preventDefault();
-    const email=document.querySelector("#crearEmail");
-    const password=document.querySelector("#crearPassword");
-    console.log(email.value,password.value);
-    var myModalEl = document.getElementById('crearModal');
-    var modal = bootstrap.Modal.getInstance(myModalEl)
-    try {
-    const respuesta=await createUserWithEmailAndPassword(auth, email.value, password.value)
+  try {
+    const respuesta = await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
     console.log(respuesta.user);
     Swal.fire({
-      title: 'La cuenta se creo con exito',
-      width: 600,
-      padding: '3em',
-      color: '#716add',
-      background: '#fff url(/images/trees.png)',
-      backdrop: `
-        rgba(0,0,123,0.4)
-      
-        left top
-        no-repeat
-      `
-    })
-  
-  email.value='';
-  password.value=''
-  modal.hide();
-  } catch(error){
-  console.log(error.code);
-  const code=error.code;
-  if(code=='auth/invalid-email'){
+      icon: "success",
+      title: "exito",
+      text: "Se Registro Correctamente!!",
+    });
+    email.value = "";
+    password.value = "";
+    modal.hide();
+  } catch (error) {
+    console.log(error.code);
+    const code = error.code;
+    if (code === "auth/invalid-email") {
       Swal.fire({
-          title: 'Correo invalido',
-          showClass: {
-            popup: 'animate__animated animate__fadeInDown'
-          },
-          hideClass: {
-            popup: 'animate__animated animate__fadeOutUp'
-          }
-        })
-  } if(code=='auth/weak-password'){
-      Swal.fire('Contraseña invalida')
-  
-  }if(code=='auth/email-already-in-use'){
-  Swal.fire('Correo ya en uso')
-  }
-  
-  }
-  
-  
-  
-   });
+        icon: "error",
 
-   const btnGoogle=document.querySelector("#BtnCAG");
-btnGoogle.addEventListener('click', async(e)=>{
-e.preventDefault();
-  const provider = new GoogleAuthProvider();
-    try {
-        const credencial=await signInWithPopup(auth, provider)
-        user=credencial.user;
-        console.log(credencial)
-        Swal.fire({
-            icon: 'success',
-        title: 'Secces',
-        text: 'You logged in',
-    })
-    if (user){
-      container.innerHTML=`<h1>${user.email}</h1>`
-      document.querySelector("#iniciar").style.display="none";
-      document.querySelector("#crear").style.display="none";
+        text: "Email Invalido!",
+      });
+    } else if (code === "auth/weak-password") {
+      Swal.fire({
+        icon: "error",
 
-      const uid=user.uid;
-    
-    }else{
+        text: "Password Invalida!",
+      });
+    } else if (code === "auth/email-already-in-user") {
+      Swal.fire({
+        icon: "error",
 
-      container.innerHTML=`<h1>no hay usuario</h1>`
+        text: "Email Ya Esta Registrado!",
+      });
     }
-    } catch (error) {
-        console.log(error)
-        Swal.fire({
-            icon: 'error',
-           title: 'Ops...',
-            text: 'Don´t is possible login whit google in this moment',
-              })
-    }
-
+  }
 });
+checarEstado();
--- 06-login/firebase.js
+++ 06-login/firebase.js
@@ -1,21 +1,20 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyCshbLZreLlWWyFuT1Ed0Ec58I05FrMp7g",
-   authDomain: "poreru-70900.firebaseapp.com",
-   projectId: "poreru-70900",
-   storageBucket: "poreru-70900.appspot.com",
-   messagingSenderId: "586564558224",
-   appId: "1:586564558224:web:2bd261590000236caba715",
-   measurementId: "G-0LYD0J7VEC"
- };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD8S40XN8LQfZAYNBoxcFvVNHfbDTKHEZQ",
+  authDomain: "proyecto-8520b.firebaseapp.com",
+  projectId: "proyecto-8520b",
+  storageBucket: "proyecto-8520b.appspot.com",
+  messagingSenderId: "1020191639166",
+  appId: "1:1020191639166:web:b25aaa4eb9cd07b17b3a2f",
+  measurementId: "G-HTRJCTTJF5"
+};
 
- // Initialize Firebase
- export const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
